5a2d940055c4e8942512a91075ac58381214190f
adamovicm <adam.thor@live.com>
2019-11-18 16:30:05 +0100

    added backup folder

---
+++ backups/09 - advanced component usage/src/components/New.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  destroyed() {
+    // eslint-disable-next-line no-console
+    console.log('Destroyed!');
+  },
+  deactivated() {
+    // eslint-disable-next-line no-console
+    console.log('Deactivated!');
+  },
+  activated() {
+    // eslint-disable-next-line no-console
+    console.log('Activated!');
+  }
+}
+</script>
+
+<style>
+
+</style>
