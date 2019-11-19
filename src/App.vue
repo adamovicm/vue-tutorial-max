d46029c2ff217709c99dd55f9809171987c4f780
adamovicm <adam.thor@live.com>
2019-11-19 10:22:48 +0100

    section 12 done

--- src/App.vue
+++ src/App.vue
@@ -1,173 +1,61 @@
 <template>
   <div class="container">
-    <form>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <h1>File a Complaint</h1>
-          <hr />
-          <div class="form-group">
-            <label for="email">Mail</label>
-            <input 
-              type="text" 
-              id="email" 
-              class="form-control" 
-              v-model="userData.email"/>
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input 
-              type="password" 
-              id="password" 
-              class="form-control" 
-              v-model.lazy="userData.password"/>
-            <p>{{ userData.password }}</p>
-          </div>
-          <div class="form-group">
-            <label for="age">Age</label>
-            <input 
-              type="number" 
-              id="age" 
-              class="form-control"
-              v-model="userData.age" />
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-          <label for="message">Message</label>
-          <br />
-          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea 
-            id="message" 
-            rows="5" 
-            class="form-control"
-            v-model="message"></textarea>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <div class="form-group">
-            <label for="sendmail">
-              <input 
-                type="checkbox" 
-                id="sendmail" 
-                value="SendMail"
-                v-model="sendMail" /> Send Mail
-            </label>
-            <label for="sendInfomail">
-              <input 
-                type="checkbox" 
-                id="sendInfomail" 
-                value="SendInfoMail"
-                v-model="sendMail" /> Send Infomail
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-          <label for="male">
-            <input 
-              type="radio" 
-              id="male" 
-              value="Male"
-              v-model="gender" /> Male
-          </label>
-          <label for="female">
-            <input 
-              type="radio" 
-              id="female" 
-              value="Female"
-              v-model="gender" /> Female
-          </label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-          <label for="priority">Priority</label>
-          <select 
-            id="priority" 
-            class="form-control"
-            v-model="selectedPriority">
-            <option 
-              v-for="(priority, index) in priorities" 
-              :key="index" 
-            >{{ priority }}</option>
-          </select>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <app-switch v-model="dataSwitch"></app-switch>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button 
-            class="btn btn-primary"
-            @click.prevent="submitted">Submit!</button>
-        </div>
-      </div>
-    </form>
-    <hr />
-    <div class="row" v-if="isSubmitted">
+    <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4>Your Data</h4>
-          </div>
-          <div class="panel-body">
-            <p>Mail: {{ userData.email }}</p>
-            <p>Password: {{ userData.password }}</p>
-            <p>Age: {{ userData.age }}</p>
-            <p style="white-space: pre">Message: {{ message }}</p>
-            <p>
-              <strong>Send Mail?</strong>
-            </p>
-            <ul>
-              <li v-for="(item, index) in sendMail" :key="index">{{ item }}</li>
-            </ul>
-            <p>Gender: {{ gender }}</p>
-            <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched: {{ dataSwitch ? 'On' : 'Off' }}</p>
-          </div>
-        </div>
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this too</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Switch from './Switch.vue';
-
 export default {
-  data() {
-    return {
-      userData: {
-        email: '',
-        password: '',
-        age: 27
-      },
-      message: 'A new Text',
-      sendMail: [],
-      gender: 'Male',
-      priorities: ['High', 'Medium', 'Low'],
-      selectedPriority: 'High',
-      dataSwitch: true,
-      isSubmitted: false
-    }
-  },
-  components: {
-    appSwitch: Switch
-  },
-  methods: {
-    submitted() {
-      this.isSubmitted = true;
+  directives: {
+    'local-highlight': {
+      bind(el, binding) {
+        var delay = 0;
+        if(binding.modifiers['delayed']) {
+          delay = 3000;
+        }
+        if(binding.modifiers['blink']) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+              if(binding.arg == 'background') {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if(binding.arg == 'background') {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
     }
   }
-};
+}
 </script>
 
 <style>
+
 </style>
